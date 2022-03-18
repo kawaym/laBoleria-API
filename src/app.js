ea5f17b48a77cd617d7a7949955d05a5057a8d15
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-03-17 23:43:25 -0300

    feat: adds create function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./routes/router.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(router);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("App listening on port" + PORT);
+  console.log("App listening on port " + PORT);
 });
---
+++ src/controllers/cakeController.js
@@ -0,0 +1,19 @@
+import connection from "../db.js";
+
+export async function createCake(req, res) {
+  const { name, price, image, description } = req.body;
+  try {
+    await connection.query(
+      `
+      INSERT INTO 
+        cakes(name, price, image, description)
+      VALUES($1, $2, $3, $4)
+    `,
+      [name, price, image, description]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
